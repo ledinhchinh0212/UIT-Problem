87d0c1386292a99dba4572d0677436ed10b97426
ledinhchinh0212 <khongbachofficial@gmail.com>
2024-05-27 00:12:00 +0700

    add features

--- UIT_OOP/Practical-Exercises/BTH7/BTH7-1.cpp
+++ UIT_OOP/Practical-Exercises/BTH7/BTH7-1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -92,7 +93,57 @@ void NhanVienSanXuat::Xuat() {
     cout << "Luong nhan vien: " << LuongNhanVien() << "\n";
 }
 
+bool cmp(NhanVien *a, NhanVien *b) {
+    return a->LuongNhanVien() > b->LuongNhanVien();
+}
+
 int main() {
 
+    cout << "Nhap so luong nhan vien trong cong ty: ";
+    int n; cin >> n;
+    NhanVien **nhanVien = new NhanVien*[n];
+    
+    for(int i = 0; i < n; i++) {
+        cout << "1. Nhan vien van phong, 2. Nhan vien san xuat: ";
+        int type; cin >> type;
+        if(type == 1) {
+            nhanVien[i] = new NhanVienVanPhong();
+        }
+        else {
+            nhanVien[i] = new NhanVienSanXuat();
+        }
+        nhanVien[i]->Nhap();
+    }
+    // luong cua tung nahn vien
+    float sum = 0.0f;
+    float maxLuong = nhanVien[0]->LuongNhanVien();      int positionMax = 0;
+    float minLuong = nhanVien[0]->LuongNhanVien();      int positionMin = 0;
+    for(int i = 0; i < n; i++) {
+        nhanVien[i]->Xuat(); cout << "\n";
+        sum = sum + nhanVien[i]->LuongNhanVien();
+        if(maxLuong < nhanVien[i]->LuongNhanVien()) {
+            maxLuong = nhanVien[i]->LuongNhanVien();
+            positionMax = i;
+        }
+        if(minLuong > nhanVien[i]->LuongNhanVien()) {
+            minLuong = nhanVien[i]->LuongNhanVien();
+            positionMin = i;
+        }
+    }
+    cout << "Tong luong ma cong ty can phai tra cho cac nhan vien: " << sum << "\n";
+    cout << "\nNhan vien co luong cao nhat: \n";
+    
+    nhanVien[positionMax]->Xuat();
+    
+    cout << "\nNhan vien co luong thap nhat: \n";
+    
+    nhanVien[positionMin]->Xuat();
+
+    cout << "\nSap xep dan sach nhan vien theo luong giam dan:\n\n";
+    sort(nhanVien, nhanVien + n, cmp);
+    for(int i = 0; i < n; i++)
+    {
+        nhanVien[i]->Xuat(); cout << "\n";
+    }
     return 0;
 }
